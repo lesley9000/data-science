71d5b40a0a23b3c816dfd8d754ca5d6c58d1148f
lesley9000 <hana.arent_18@mailbox.org>
2021-05-24 19:14:04 +0200

    Update 1

--- session06_shiny-app.R
+++ session06_shiny-app.R
@@ -22,79 +22,79 @@ ui <- tagList(
              header = HTML("<meta charset = 'utf-8'"), # UTF-8
              theme = shinytheme("cerulean"),         # maritimes Theme
              fluid = TRUE,                           # fluidPage
-                 
-                 # 1. Tab mit Schiff-Icon
-                 tabPanel("Prognose", icon = icon("ship"),
-                          
-                          # Layout der Sidebar
-                          sidebarPanel(
-                            
-                            # Überschrift für Parameter
-                            tags$label(h3("Dateneingabe", style = "margin-bottom: 20px;")),
-                           
-                            # Features eingeben
-                            sliderInput("pclass", "Passagierklasse:",
-                                        min = 1, max = 3,
-                                        value = 2),
-                            
-                            radioButtons("sex", inline = TRUE, "Geschlecht:",
-                                         c("Frau" = 1,
-                                           "Mann" = 0)),
-                            
-                            numericInput("age", "Alter:",
-                                         value = "0"),
-                            
-                            radioButtons("relatives", inline = TRUE, "Mit Familie reisend?",
-                                         c("Ja" = 1,
-                                           "Nein" = 0)),
-                            
-                            selectInput("embarked", "Einstiegshafen:",
-                                        c("Cherbourg" = 0,
-                                          "Southampton" = 1,
-                                          "Queenstown" = 2)),
-                            
-                            # Action-Button löst observeEvent() aus
-                            actionButton("action", label = "Wird die Person überleben?", class = "btn btn-primary"),
-                          
-                            # Margin für Sidebar
-                            style = "margin-top: 15px;"  
-                            
-                          ), # Ende Sidebar Panel
-                          
-                          # Layout des Main Panels
-                          mainPanel(
-                            
-                            # Überschrift für Prognose
-                            tags$label(h3("Wer überlebt die Titanic?", style = "margin-bottom: 20px;")),
-                           
-                            # Erklärtext
-                            HTML("
+             
+             # 1. Tab mit Schiff-Icon
+             tabPanel("Prognose", icon = icon("ship"),
+                      
+                      # Layout der Sidebar
+                      sidebarPanel(
+                        
+                        # Überschrift für Parameter
+                        tags$label(h3("Dateneingabe", style = "margin-bottom: 20px;")),
+                        
+                        # Features eingeben
+                        sliderInput("pclass", "Passagierklasse:",
+                                    min = 1, max = 3,
+                                    value = 2),
+                        
+                        radioButtons("sex", inline = TRUE, "Geschlecht:",
+                                     c("Frau" = 1,
+                                       "Mann" = 0)),
+                        
+                        numericInput("age", "Alter:",
+                                     value = "0"),
+                        
+                        radioButtons("relatives", inline = TRUE, "Mit Familie reisend?",
+                                     c("Ja" = 1,
+                                       "Nein" = 0)),
+                        
+                        selectInput("embarked", "Einstiegshafen:",
+                                    c("Cherbourg" = 0,
+                                      "Southampton" = 1,
+                                      "Queenstown" = 2)),
+                        
+                        # Action-Button löst observeEvent() aus
+                        actionButton("action", label = "Wird die Person überleben?", class = "btn btn-primary"),
+                        
+                        # Margin für Sidebar
+                        style = "margin-top: 15px;"  
+                        
+                      ), # Ende Sidebar Panel
+                      
+                      # Layout des Main Panels
+                      mainPanel(
+                        
+                        # Überschrift für Prognose
+                        tags$label(h3("Wer überlebt die Titanic?", style = "margin-bottom: 20px;")),
+                        
+                        # Erklärtext
+                        HTML("
                               <p>Füllen Sie das Formular aus, um herauszufinden, ob die fragliche Person das Unglück übersteht oder nicht.</p>
                               <p>Der Wert <i>p(überlebt)</i> gibt die Überlebenswahrscheinlichkeit an, während der Wert <i>p(verstorben)</i> die Wahrscheinlichkeit ausdrückt, dass die Person verstirbt.</p>
                               <p><strong>Beispiel:</strong> Ein <i>p(überlebt)</i>-Wert von 0.43 sagt aus, dass die Wahrscheinlichkeit des Überlebens bei 43% liegt.</p>
                                  "),
-                            
-                            # Output: Anzeige der Prognose
-                            tags$div(
-                              tableOutput("value1"),
-                              style = "margin-top: 20px;"
-                              )
-                            
-                          ) # Ende Main Panel
-                          
-                 ), # Ende 1. Tab
-                 
-                 # 2. Tab mit Info-Icon
-                 tabPanel("FAQ", icon = icon("info-circle"),
-                         
-                          # Layout des Main Panels
-                          mainPanel(
-                            
-                            # Überschrift
-                            tags$label(h3("Häufig gestellte Fragen", style = "margin-bottom: 20px;")),
-                           
-                            # Fragen und Antworten
-                            HTML("
+                        
+                        # Output: Anzeige der Prognose
+                        tags$div(
+                          tableOutput("value1"),
+                          style = "margin-top: 20px;"
+                        )
+                        
+                      ) # Ende Main Panel
+                      
+             ), # Ende 1. Tab
+             
+             # 2. Tab mit Info-Icon
+             tabPanel("FAQ", icon = icon("info-circle"),
+                      
+                      # Layout des Main Panels
+                      mainPanel(
+                        
+                        # Überschrift
+                        tags$label(h3("Häufig gestellte Fragen", style = "margin-bottom: 20px;")),
+                        
+                        # Fragen und Antworten
+                        HTML("
                               <h4>Wie funktioniert das Orakel?</h4>
                               <p style = 'margin-bottom: 25px;'>
                                 Hinter dem Orakel verbirgt sich der Klassifikations-Algorithmus <i>Support Vector Machines</i> (SVM).
@@ -107,13 +107,13 @@ ui <- tagList(
                                 Sofern das Formular ausgefüllt wurde, erscheint an dieser Stelle ein Entscheidungsbaum, der Aufschluss darüber gibt, welche Parameter besonders großen Einfluss auf das Ergebnis haben.
                               </p>
                             "),
-                            
-                            # Output: Anzeige des Decision Trees
-                            plotOutput("value2")
-                            
-                          ) # Ende Main Panel
-                          
-                 ) # Ende 2. Tab
+                        
+                        # Output: Anzeige des Decision Trees
+                        plotOutput("value2")
+                        
+                      ) # Ende Main Panel
+                      
+             ) # Ende 2. Tab
              
   ), # Ende Navbar Page
   
